c45e6387a5c0022a1c3ba85eb58093a2f53df4a6
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2023-02-02 20:59:11 -0800

    Daily Report Assignment Feb02/23
    Daily Report Assignment Submission
    Your job is to create a daily report for students of Academy of Learning Career College.
    
    Requirements
    
    The program must start by printing “Academy of Learning Career College”.
    
    The next line must be “Student Daily Report.”
    
    You must ask and save your data according to its proper data type and with clearly labeled variables. 
    
    The DR must contain the following questions:
    
    What is your name?
    
    What course are you on?
    
    What page number?
    Do you need help with anything? Please answer “true” or “false”.
    
    Were there any positive experiences you’d like to share? Please give specifics.
    
    Is there any other feedback you’d like to provide? Please be specific.
    
    How many hours did you study today?
    
    After all of the questions are asked, print, “Thank you for your answers. An Instructor will respond shortly. Have a great day!” This will be the end of the program.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ Daily Report/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace Daily_Report
+{
+    class Program
+    {
+        static void Main()
+        {
+            //First Line
+                Console.WriteLine("Academy of Learning Career College");
+            //Second Line
+                Console.WriteLine("Student Daily Report");
+            //Console.WriteLine("Please save your data according to its proper data type");
+            //Your Name?
+                Console.WriteLine("What is your name?");
+                string yourName = Console.ReadLine();
+            //Responsive Reply + Next Question 
+                Console.WriteLine("Hello " + yourName + ". What course are you on?");
+                string yourCourse = Console.ReadLine();
+            //Question C Page #
+                Console.WriteLine("Wow " + yourCourse + "! What page number?");
+                string yourPage = Console.ReadLine();
+            //Converting string to integer 
+                int currentPage = Convert.ToInt32(yourPage);
+            // Next question to answer with a boolean 
+                Console.WriteLine(yourPage + " Do you need help with anything? Please answer \"true\" or \"false\"");
+                string trueorfalse = Console.ReadLine();
+            //Converting String to Boolean
+                 bool torf = Convert.ToBoolean(trueorfalse);
+            //Last few questions
+            Console.WriteLine("Are there any positive experiences you'd like to share?");
+            string experiences = Console.ReadLine();
+            Console.WriteLine("Is there other feedback you'd like to provide?");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string yourTodayHours = Console.ReadLine();
+            //Convert String to Int
+            int currentHours = Convert.ToInt32(yourTodayHours);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
