1126cf65aa3b73cd4374525e4679f8299860771b
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2023-02-02 18:08:39 -0800

    Accounting Assignment Feb 2/23
    When the program is run, Welcome to Acme Accounting Systems is printed to the console.
    
    Below that, Remember, we’re “accounting” on you! is printed to the console.

---
+++ Accounting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to ACME Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+
+        }
+    }
+}
