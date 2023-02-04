c2e5d653f126502c890e1b9d575e828b929fb114
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2023-02-04 13:58:43 -0800

    Mate&ComparisonOperators Feb 04/23

---
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main()
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //int otherDifference = 22 - 12;
+            //int combined = difference + otherDifference;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int quotient = 100 / 5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            // //Program will round if there is a decimal(or remainder) 
+            // //If you want to be more specific(decimal answers) then use 
+            // // a different datatype. 
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            // //Remainders. % Modulas Operator
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            // //output is 1. Which is 11/2's remainder. 
+
+            //int num1 = 200 / 3;
+            //Console.WriteLine(num1);
+            //Console.ReadLine();
+
+            // //Comparison Operators
+
+            //bool trueOrfalse = 12 < 5;
+            //Console.Write(trueOrfalse.ToString());
+            //Console.ReadLine();
+
+        //    int roomTemperature = 70;
+        //    int currentTemperature = 70;
+
+        //    //bool isWarm = currentTemperature (== != >= <=) roomTemperature;
+        //    bool isWarm = currentTemperature != roomTemperature;
+        //    Console.WriteLine(isWarm);
+        //    Console.ReadLine();
+        //
+        }
+    }
+}
