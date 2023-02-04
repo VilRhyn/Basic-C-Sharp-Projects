612b741bf855de35366256642006dde1670655a2
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2023-02-04 09:13:09 -0800

    MathChallenge Feb 04
    Math Challenge
    In Visual Studio, perform the following with C#:
    
    Add two numbers and display the output in the console.
    
    Subtract two numbers and display the output in the console.
    
    Multiply two numbers and display the output in the console.
    
    Divide two numbers and display the output in the console.

---
+++ MathChallenge/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("5 plus 7 is: ");
+            Console.ReadLine();
+            int num1 = 5;
+            int num2 = 7;
+            int total = num1 + num2;
+            Console.WriteLine(total);
+            Console.ReadLine();
+
+            Console.WriteLine("5 minus 7 is: ");
+            Console.ReadLine();
+            int num3 = 5;
+            int num4 = 7;
+            int difference = num3 - num4;
+            Console.WriteLine(difference);
+            Console.ReadLine();
+
+            Console.WriteLine("5 times 7 is: ");
+            Console.ReadLine();
+            int num5 = 5;
+            int num6 = 7;
+            int product = num5 * num6;
+            Console.WriteLine(product);
+            Console.ReadLine();
+
+            Console.WriteLine("15 divided by 5 is: ");
+            Console.ReadLine();
+            int num7 = 15;
+            int num8 = 5;
+            int quotient = num7 / num8;
+            Console.WriteLine(quotient);
+            Console.ReadLine();
+
+            Console.WriteLine("remainder of 7/3 is: ");
+                Console.ReadLine();
+            int num9 = 7;
+            int num0 = 3;
+            int remainder = num9 % num0;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+        }
+    }
+}
