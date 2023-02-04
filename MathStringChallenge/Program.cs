41e7b8304e4dfb4a1fd3b6d1d59fff3117f8c3fc
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2023-02-04 09:33:24 -0800

    MathString Challenge Feb4/23
    Math String Challenge
    Using Visual Studio, perform a math operation on a string.

---
+++ MathStringChallenge/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MathStringChallenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            String firstName = "Vil";
+            String lastName = "Rhyn";
+            Console.WriteLine(firstName + lastName);
+            Console.ReadLine();
+        }
+    }
+}
