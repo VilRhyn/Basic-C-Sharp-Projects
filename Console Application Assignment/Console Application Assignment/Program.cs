db2e385211cf20df45e01ed8560c7fbe8747ef04
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2023-02-04 15:16:51 -0800

    Console App Assignment Feb 04/23
    Console Application Assignment
    In Visual Studio, create a console application that does the following:
    
    Takes an input from the user, multiplies it by 50, then prints the result to the console. (Note: make sure your code can take inputs larger than 10,000,000).
    
    Takes an input from the user, adds 25 to it, then prints the result to the console.
    
    Takes an input from the user, divides it by 12.5, then prints the result to the console.
    
    Takes an input from the user, checks if it is greater than 50, then prints the true/false result to the console.
    
    Takes an input from the user, divides it by 7, then prints the remainder to the console (tip: think % operator).
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
    
    Upload your code to GitHub.
    
    Note: The code in this assignment does not need to be reviewed by an Instructor, so you will not be submitting a link to GitHub.

---
+++ Console Application Assignment/Console Application Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Console_Application_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //User input times 50
+            Console.WriteLine("Enter a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number times 50 is " + userNumber * 50);
+            Console.ReadLine();
+
+            //User input plus 25 
+            Console.WriteLine("Enter another number: ");
+            int anotherNumber = Convert.ToInt32(Console.ReadLine());
+            int plus25 = 25;
+            Console.WriteLine("Your number plus 25");
+            Console.WriteLine(anotherNumber + plus25);
+            Console.ReadLine();
+
+            //User input divided by 12.5
+            Console.WriteLine("Pick a number: ");
+            double Num1 = Convert.ToDouble(Console.ReadLine());
+            double dvd = 12.5;
+            Console.WriteLine("This number divided by 12.5 is ");
+            Console.WriteLine(Num1 / dvd);
+            Console.ReadLine();
+
+            //Checking to see if input is greater than 50
+            Console.WriteLine("You know what to do *(Pick a number)*");
+            int Num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("This number is greater than 50(?)");
+            bool trueOrfalse = Num2 > 50;
+            Console.Write(trueOrfalse.ToString());
+            Console.ReadLine();
+
+            //Divide by 7 and see if there is a remainder (Modulas Operator)
+            Console.WriteLine("Are you sick of picking numbers yet? Too bad. Enter another");
+            double Num3 = Convert.ToDouble(Console.ReadLine());
+            double Num4 = 7;
+            Console.WriteLine("If divided by 7 has a remainder of " + Num3 % Num4);
+            Console.ReadLine();
+
+        }
+    }
+}
