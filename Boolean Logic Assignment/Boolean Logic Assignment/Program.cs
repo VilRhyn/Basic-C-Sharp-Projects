d53b99e6ec3ec8ef6146c5be047215f3303aa482
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2023-02-06 15:21:48 -0800

    Boolean Logic Assignment Feb 06/23
    Boolean Logic Assignment 
    
    Your job is to create a basic approval program for car insurance. It will ask the applicant some questions and return a “true” or “false” at the end to determine whether or not the applicant qualified for car insurance.
    
    Requirements
    
    Your application must ask these questions:
    
    What is your age?
    
    Have you ever had a DUI?
    
    How many speeding tickets do you have?
    
    Use the following qualification rules to determine if the applicant qualifies for car insurance:
    
    Applicants must be over 15 years old.
    
    Applicants must not have any DUI’s.
    
    Applicants must not have more than 3 speeding tickets.
    
    Print the result of the boolean expression created from the above business rules.
    
    Example output:
    
    What is your age?
    32
    Have you ever had a DUI?
    false
    How many speeding tickets do you have?
    1
    Qualified?
    true

---
+++ Boolean Logic Assignment/Boolean Logic Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Boolean_Logic_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Car Insurance Approvale Questionaire");
+            //How old are you ? 
+            Console.WriteLine("What is your age?");
+            //Converts user input to int data type 
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            //Have you had a DUI
+            Console.WriteLine("Have you ever had a DUI? TRUE or FALSE");
+            //Converts user input to boolean data type
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+            //How many speeding tickets 
+            Console.WriteLine("How many speeding tickets have you had?");
+            //Converts user input to int data type
+            int SpdTkt = Convert.ToInt32(Console.ReadLine());
+
+            //variable result to be true = variable age must be greater than 15
+            //                           = variable DUI must be false 
+            //                           = variable SpdTkt must be less than or equal to 3
+            bool result = (Age > 15 && DUI == false && SpdTkt <= 3);
+
+            //Console Writes Qualified: + either true or false based on the above paramaters
+            Console.WriteLine("Qualified:" + result);
+            Console.ReadLine();
+        }
+    }
+}
