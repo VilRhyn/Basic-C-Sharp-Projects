1276e8c9ade03c3e01c947807a28d14e0d36c389
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2023-02-06 21:14:37 -0800

    Resubmit Branching Assignment Feb 06/23
    Resubmitting Branching Assignment with Code Comments.

--- Branching Sub Assignment/Branching Sub Assignment/Program.cs
+++ Branching Sub Assignment/Branching Sub Assignment/Program.cs
@@ -10,13 +10,15 @@ namespace Branching_Sub_Assignment
             Console.WriteLine("Please enter the package weight.");
             int packageWeight = Convert.ToInt32(Console.ReadLine());
 
+            //This will clost  the program if the weight is more than 50
             if (packageWeight > 51)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
+            //Continue Program is weight is less than 50 
             else if(packageWeight < 51)
             {
-
+                //Creating user input variables
                 Console.WriteLine("Please enter the package width");
                 int packageWidth = Convert.ToInt32(Console.ReadLine());
 
@@ -26,9 +28,10 @@ namespace Branching_Sub_Assignment
                 Console.WriteLine("Please enter the package length");
                 int packageLength = Convert.ToInt32(Console.ReadLine());
 
-
+                //creating a math equation for timesing the width, heigh, and length together, then timesing it by the weight of the package 
                 int packageCost = (packageWidth * packageHeight * packageLength) * packageWeight;
 
+                //Shows user how much estimated cost will be 
                 Console.WriteLine("Your estimated total for shipping this package is $" + packageCost / 100 + " Thank you!"); 
             }
 
