68924807fb873cf4cd1a1b1f9b10fb8e6e61c2d0
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2023-02-07 15:22:23 -0800

    String Assignment Feb 07/23
    String Assignment
    Create a console app that does/includes the following:
    
    Concatenates three strings.
    
    Converts a string to uppercase.
    
    Creates a Stringbuilder and builds a paragraph of text, one sentence at a time.

---
+++ String Assignment/String Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace String_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Building a string via string builder. String is called myStringBuilder
+            StringBuilder myStringBuilder = new StringBuilder("Time to party,", 25);//Initial display
+           myStringBuilder.Append("and party...");//Adds text to String
+            myStringBuilder.Append("and party some more");//Adds more text to growing string
+            Console.WriteLine(myStringBuilder);
+            
+
+            //Initializing
+            string hi = "Hello, World";
+            string name = "Melanie";
+            string num = "seven";
+            string color = "Sunset";
+
+            Console.WriteLine(hi.ToUpper());//Converts Hello, World to uppercase
+            Console.WriteLine(name + "'s favorite number is: " + num + ", and her favorite colour is: " + color);//Concatenating 3 strings
+
+            Console.ReadLine();
+        }
+    }
+}
