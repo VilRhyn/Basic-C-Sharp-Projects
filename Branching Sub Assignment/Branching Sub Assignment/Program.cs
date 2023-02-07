343ae9e39ba60fba6c3e93bc461ac46f438ba636
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2023-02-07 10:22:58 -0800

    Resubmit Branching Sub Assignment  Feb 07/23
    + code for dimensions

--- Branching Sub Assignment/Branching Sub Assignment/Program.cs
+++ Branching Sub Assignment/Branching Sub Assignment/Program.cs
@@ -9,34 +9,41 @@ namespace Branching_Sub_Assignment
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.WriteLine("Please enter the package weight.");
             int packageWeight = Convert.ToInt32(Console.ReadLine());
-
-            //This will clost  the program if the weight is more than 50
-            if (packageWeight > 51)
+            
+            //shuts down the program if weight is too heavy
+            if (packageWeight >= 50) 
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
-            }
-            //Continue Program is weight is less than 50 
-            else if(packageWeight < 51)
+            }    
+
+           //user input variables 
+            Console.WriteLine("Please enter the package width");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length");
+            int packageLength = Convert.ToInt32(Console.ReadLine());
+
+            //creating packageDimension variable 
+            int packageDimensions = packageWidth + packageHeight + packageLength;
+          
+            if (packageDimensions < 50)
             {
-                //Creating user input variables
-                Console.WriteLine("Please enter the package width");
-                int packageWidth = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Please enter the package height");
-                int packageHeight = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Please enter the package length");
-                int packageLength = Convert.ToInt32(Console.ReadLine());
-
                 //creating a math equation for timesing the width, heigh, and length together, then timesing it by the weight of the package 
                 int packageCost = (packageWidth * packageHeight * packageLength) * packageWeight;
 
                 //Shows user how much estimated cost will be 
-                Console.WriteLine("Your estimated total for shipping this package is $" + packageCost / 100 + " Thank you!"); 
+                Console.WriteLine("Your estimated total for shipping this package is $" + packageCost / 100 + " Thank you!");
             }
 
-            Console.ReadLine();
-
+            //if dimensions greater than or equal to 50 than write : 
+            else if (packageDimensions >= 50) ;
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+                    Console.ReadLine();
         }
     }
 }
